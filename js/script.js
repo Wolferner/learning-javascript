893363c422b573e2079be0353c3524a4aeb17009
Wolferner <m.wolfern@yandex.com>
2023-12-08 23:45:47 +0200

    second

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 "use strict";
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
@@ -31,12 +8,23 @@ const personalMovieDB = {
   genres: [],
   privat: false,
 };
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const lastFilmRating = prompt("На сколько оцените его?", "");
-const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
-const lastFilmRating2 = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[lastFilm] = lastFilmRating;
-personalMovieDB.movies[lastFilm2] = lastFilmRating2;
+first: for (let i = 0; i < 2; i++) {
+  let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+  let lastFilmRating = prompt("На сколько оцените его?", "");
+  if (+lastFilm.length !== 0 || +lastFilm.length < 50) {
+    personalMovieDB.movies[lastFilm] = lastFilmRating;
+  }
+
+  if (personalMovieDB.count < 10) {
+    console.log("malo");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("mnogo");
+  } else if (personalMovieDB.count > 30) {
+    console.log("kinoman");
+  } else {
+    console.log("some error");
+  }
+}
 
 console.log(personalMovieDB);
